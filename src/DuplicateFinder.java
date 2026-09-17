e08389c74c37946236ee3ce4b07a6cbb19ba1436
Jvzqz13 <142378858+Jvzqz13@users.noreply.github.com>
2026-09-16 20:59:56 -0400

    created file, fingerprint Map

--- src/DuplicateFinder.java
+++ src/DuplicateFinder.java
@@ -3,26 +3,28 @@ import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class DuplicateFinder {
-    public static void main(String[]args){
+    public static void main(String[] args) {
 
         File selectedFolder = new File("C:\\Users\\jvzqz\\OneDrive\\Pictures\\finder_example");
 
         File[] fileList = selectedFolder.listFiles();
+        Map<File, String> fingerprints = new HashMap<>();
 
-        for (File file : fileList){
+        for (File file : fileList) {
             String fileName = file.getName().toLowerCase();
-            if(
-                (
-                 fileName.endsWith(".jpg") ||
-                 fileName.endsWith(".jpeg") ||
-                 fileName.endsWith(".png")
-                ) &&
-                 file.isFile()
-            )
-            {
-                try{
+            if (
+                    (
+                            fileName.endsWith(".jpg") ||
+                                    fileName.endsWith(".jpeg") ||
+                                    fileName.endsWith(".png")
+                    ) &&
+                            file.isFile()
+            ) {
+                try {
                     // READS IMAGE
                     BufferedImage image = ImageIO.read(file);
 
@@ -31,19 +33,18 @@ public class DuplicateFinder {
 
                     //ALLOWS IMAGE TO BE CREATED IN RESIZEDIMAGE
                     Graphics2D graphics = resizedImage.createGraphics();
-                    graphics.drawImage(image, 0,0, 8, 8,  null);
+                    graphics.drawImage(image, 0, 0, 8, 8, null);
                     graphics.dispose();
 
                     int[] brightnessValues = new int[64];
                     int total = 0;
 
 
-
-                    for(int y = 0; y < resizedImage.getHeight(); y++){
-                        for (int x = 0; x < resizedImage.getWidth(); x++){
+                    for (int y = 0; y < resizedImage.getHeight(); y++) {
+                        for (int x = 0; x < resizedImage.getWidth(); x++) {
                             int index = (y * 8) + x;
 
-                            int pixel = resizedImage.getRGB(x,y);
+                            int pixel = resizedImage.getRGB(x, y);
                             int red = (pixel >> 16) & 0xFF;
                             int green = (pixel >> 8) & 0xFF;
                             int blue = (pixel) & 0xFF;
@@ -53,33 +54,33 @@ public class DuplicateFinder {
 
                         }
                     }
-                    for (int brightness : brightnessValues){
-                        total+= brightness;
+
+                    for (int brightness : brightnessValues) {
+                        total += brightness;
                     }
 
-                    int averageBrightness = total/64;
+                    int averageBrightness = total / 64;
 
                     String fingerPrint = "";
 
                     //CREATES A FINGERPRINT OF THE IMAGE
-                    for(int brightness : brightnessValues){
-                        if(brightness >= averageBrightness){
+                    for (int brightness : brightnessValues) {
+                        if (brightness >= averageBrightness) {
                             fingerPrint += "1";
                         } else {
                             fingerPrint += "0";
                         }
                     }
-                    System.out.println(file.getName() + " " + fingerPrint);
 
+                    fingerprints.put(file, fingerPrint);
 
-                }catch (IOException e){
+                } catch (IOException e) {
                     System.out.println("Could not read: " + file.getName());
 
                 }
-
             }
         }
-
+        System.out.println(fingerprints.size());
 
     }
 }
