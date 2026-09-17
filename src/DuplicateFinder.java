256510a6f2f9652b0ba92386415a1e7981abad5a
Jvzqz13 <142378858+Jvzqz13@users.noreply.github.com>
2026-09-16 22:35:50 -0400

    CREATED LABELS EACH SECTION

--- src/DuplicateFinder.java
+++ src/DuplicateFinder.java
@@ -11,11 +11,16 @@ public class DuplicateFinder {
 
         File selectedFolder = new File("C:\\Users\\jvzqz\\OneDrive\\Pictures\\finder_example");
 
+        //GET ALL FILES FROM THE SELECTED FOLDER
         File[] fileList = selectedFolder.listFiles();
+
+        //STORES EACH IMAGE FILE AND ITS GENERATED FINGERPRINT
         Map<File, String> fingerprints = new HashMap<>();
 
+        //LOOPS THROUGH EVERY FILE IN THE FOLDER
         for (File file : fileList) {
             String fileName = file.getName().toLowerCase();
+                // CHECKS THE FILE IS A SUPPORTED IMAGE TYPE
             if (
                     (
                             fileName.endsWith(".jpg") ||
@@ -31,19 +36,23 @@ public class DuplicateFinder {
                     // CREATES 8X8 EMPTY IMAGE
                     BufferedImage resizedImage = new BufferedImage(8, 8, BufferedImage.TYPE_INT_RGB);
 
-                    //ALLOWS IMAGE TO BE CREATED IN RESIZEDIMAGE
+                    //ALLOWS IMAGE TO BE CREATED IN RESIZEDIMAGE // RESIZES THE IMAGE INTO THE 8X8 IMAGE
                     Graphics2D graphics = resizedImage.createGraphics();
                     graphics.drawImage(image, 0, 0, 8, 8, null);
                     graphics.dispose();
 
+                    //STORES THE BRIGHTNESS OF EACH OF THE 64 PIXELS
                     int[] brightnessValues = new int[64];
                     int total = 0;
 
-
+                    // LOOPS THROUGH EVERY PIXEL IN THE 8X8 IMAGE
                     for (int y = 0; y < resizedImage.getHeight(); y++) {
                         for (int x = 0; x < resizedImage.getWidth(); x++) {
+
+                            //CONVERTS X/Y POSITION INTO AN ARRAY INDEX
                             int index = (y * 8) + x;
 
+                            // GETS RGB VALUES AND CALCULATES PIXEL BRIGHTNESS
                             int pixel = resizedImage.getRGB(x, y);
                             int red = (pixel >> 16) & 0xFF;
                             int green = (pixel >> 8) & 0xFF;
@@ -55,15 +64,17 @@ public class DuplicateFinder {
                         }
                     }
 
+                    // ADDS ALL PIXEL BRIGHTNESS VALUES TOGETHER
                     for (int brightness : brightnessValues) {
                         total += brightness;
                     }
 
+                    // CALCULATES THE IMAGES VALUES TOGETHER
                     int averageBrightness = total / 64;
 
                     String fingerPrint = "";
 
-                    //CREATES A FINGERPRINT OF THE IMAGE
+                    //CREATES A FINGERPRINT OF THE IMAGE BASED ON AVERAGE BRIGHTNESS
                     for (int brightness : brightnessValues) {
                         if (brightness >= averageBrightness) {
                             fingerPrint += "1";
@@ -72,6 +83,7 @@ public class DuplicateFinder {
                         }
                     }
 
+                    // STORES: IMAGE FILE -> FINGERPRINT
                     fingerprints.put(file, fingerPrint);
 
                 } catch (IOException e) {
@@ -80,6 +92,7 @@ public class DuplicateFinder {
                 }
             }
         }
+        // PRINTS HOW MANY IMAGE FINGERPRINTS WERE CREATED
         System.out.println(fingerprints.size());
 
     }
